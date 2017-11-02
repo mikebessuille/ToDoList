4a1d2d57b995ea520319c02ec09c32b9a32ed0d9
Mike Bessuille <mikebessuille@hotmail.com>
2017-11-02 15:06:59 -0400

    Bottom Panel and sizer started, but it doesn't work correctly; it prevents the ListPanel from creating a scrollable list, and expands it right away.  New code is commented out for now.

--- ToDoList/main.cpp
+++ ToDoList/main.cpp
@@ -23,9 +23,14 @@ private:
 	void OnExit(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
 
+private:
+	void CreateMenu();
+
 // member variables
 private:
-	ListPanel *m_panel;
+	ListPanel *m_ListPanel;
+	wxPanel *m_BottomPanel;
+	wxButton *m_AddButton;
 
 private:
 	wxDECLARE_EVENT_TABLE();
@@ -54,31 +59,30 @@ bool MyApp::OnInit()
 MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
 	: wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-	wxMenu *menuFile = new wxMenu;
-	menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-		"Help string shown in status bar for this menu item");
-	menuFile->AppendSeparator();
-	menuFile->Append(wxID_EXIT);
-	wxMenu *menuHelp = new wxMenu;
-	menuHelp->Append(wxID_ABOUT);
-	wxMenuBar *menuBar = new wxMenuBar;
-	menuBar->Append(menuFile, "&File");
-	menuBar->Append(menuHelp, "&Help");
-	SetMenuBar(menuBar);
-	/*
-	CreateStatusBar();
-	SetStatusText("Welcome to wxWidgets!");
-	*/
+	CreateMenu();
 	SetMinSize(wxSize(300, 350)); // Of the whole MyFrame (which contains the whole app)
 
 	// The ListPanel which contains all the ToDoItems
-	m_panel = new ListPanel(this);
+	m_ListPanel = new ListPanel(this);
 
-	// TODO: Create a bottom window / panel, with an "Add" button
+	/*
+	// Create a bottom window / panel, with an "Add" button
+	m_BottomPanel = new wxPanel(this);
+	wxButton *m_AddButton = new wxButton(m_BottomPanel, wxID_ANY, wxT("Add"));
+	*/
 
-	// TODO: add the ListPanel and bottom panel into a new vertical sizer 
-
-	// TODO: call SetSizer() into this MyFrame()
+	// Add the ListPanel and bottom panel into a new vertical sizer 
+	// Flags: Vertically not stretchable (0 to constructor)
+	// Horizontally stretchable (Expand())
+	// Left-aligned
+	// with border width 5.
+	/*
+	wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+	sizer->Add(m_ListPanel, wxSizerFlags(1).Left().Expand()); // No Border
+	sizer->Add(m_BottomPanel, wxSizerFlags(0).Left().Border(wxALL, 5)); // Cannot expand vertically or horizontally
+	sizer->SetSizeHints(this); // Should never resize smaller than the initial window... ?
+	SetSizer(sizer); // use the sizer for layout
+	*/
 }
 
 void MyFrame::OnExit(wxCommandEvent& event)
@@ -95,4 +99,24 @@ void MyFrame::OnAbout(wxCommandEvent& event)
 void MyFrame::OnHello(wxCommandEvent& event)
 {
 	wxLogMessage("Hello world from wxWidgets!");
+}
+
+
+void MyFrame::CreateMenu()
+{
+	wxMenu *menuFile = new wxMenu;
+	menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+		"Help string shown in status bar for this menu item");
+	menuFile->AppendSeparator();
+	menuFile->Append(wxID_EXIT);
+	wxMenu *menuHelp = new wxMenu;
+	menuHelp->Append(wxID_ABOUT);
+	wxMenuBar *menuBar = new wxMenuBar;
+	menuBar->Append(menuFile, "&File");
+	menuBar->Append(menuHelp, "&Help");
+	SetMenuBar(menuBar);
+	/*
+	CreateStatusBar();
+	SetStatusText("Welcome to wxWidgets!");
+	*/
 }
